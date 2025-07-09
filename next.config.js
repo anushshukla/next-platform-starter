7bdc8a5dec08b9d63fcf231a06e2c8006c81c8fa
Anush Shukla <34502156+anushshukla@users.noreply.github.com>
2025-07-09 17:04:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: "default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src *.visualwebsiteoptimizer.com;",
+              },
+            ],
+          },
+        ]
+    },
 };
 
 module.exports = nextConfig;
