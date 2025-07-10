f0ffcce2966bf1c0fbc9824030caed6d58c56d53
Anush Shukla <34502156+anushshukla@users.noreply.github.com>
2025-07-10 10:55:40 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,6 +14,11 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <meta http-equiv="Content-Security-Policy" content="
+                  script-src 'self' 'unsafe-inline' *.visualwebsiteoptimizer.com;
+                  connect-src 'self';
+                  worker-src 'self' blob:;
+                ">
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
