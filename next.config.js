1311fb9b605f6a8a25d355b0ee58649c93413c0b
Anush Shukla <34502156+anushshukla@users.noreply.github.com>
2025-09-10 02:12:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,15 @@ const nextConfig = {
     reactStrictMode: true,
     async headers() {
         return [
-          {
-            source: '/(.*)',
-            headers: [
-              {
-                key: 'Content-Security-Policy',
-                value: "worker-src 'self' blob:;script-src 'self' 'unsafe-inline' *.visualwebsiteoptimizer.com app.vwo.com;",
-              },
-            ],
-          },
+          // {
+          //   source: '/(.*)',
+          //   headers: [
+          //     {
+          //       key: 'Content-Security-Policy',
+          //       value: "worker-src 'self' blob:;script-src 'self' 'unsafe-inline' *.visualwebsiteoptimizer.com app.vwo.com;",
+          //     },
+          //   ],
+          // },
         ]
     },
 };
