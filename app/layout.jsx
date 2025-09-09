aa892b5a84aed4342fa7001404d12056ac2c1dbe
Anush Shukla <34502156+anushshukla@users.noreply.github.com>
2025-09-10 02:04:14 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,6 +26,7 @@ export default function RootLayout({ children }) {
                   worker-src 'self' blob:;
                 " />
                 <link rel="preconnect" href="https://dev.visualwebsiteoptimizer.com" />
+                <script type="text/javascript" async src="https://cdn.mida.so/js/optimize.js?key=g23zJvlwd1qJ6dEk7PbV9W"></script>
                 <script type="text/javascript" id="vwoCode" dangerouslySetInnerHTML={{ __html: smartCode }} />
             </head>
             <body className="antialiased text-white bg-blue-900">
